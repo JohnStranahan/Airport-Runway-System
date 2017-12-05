ea2c64a8ad7a505a5b72b782f52d524e7ef49d57
Tapan Soni <32372516+TapanSoni@users.noreply.github.com>
2017-12-04 20:32:18 -0500

    Update AOSLArrayBased.java

--- AOSLArrayBased.java
+++ AOSLArrayBased.java
@@ -1,3 +1,13 @@
+/**
+ * Purpose: Data Structure and Algorithms Fall 2017 Final Project
+ * Status: Complete and throughly tested
+ * Last Update: 12/4/17
+ * Submitted: 12/5/17
+ * Comment: Test suite and sample run attached
+ * @author Tapan Soni
+ * @author John Stranahan
+ * @version 2017.4.12
+ */
 
 public class AOSLArrayBased implements AscendinglyOrderedStringListInterface
 {
@@ -119,7 +129,7 @@ public class AOSLArrayBased implements AscendinglyOrderedStringListInterface
 		} // END WHILE
 		
 		return (0 - low - 1);
-	} // END add() method
+	} // END search() method
 
 	public void clear() 
 	{
