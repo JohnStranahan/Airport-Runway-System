8767942aeb701b1c628eb9c06c7325d08f73a89b
JohnStranahan <jasballpythons@msn.com>
2017-12-04 20:33:19 -0500

    Update ListArrayBasedGeneric.java

--- ListArrayBasedGeneric.java
+++ ListArrayBasedGeneric.java
@@ -1,3 +1,14 @@
+/**
+ * Purpose: Data Structure and Algorithms Fall 2017 Final Project
+ * Status: Complete and throughly tested
+ * Last Update: 12/4/17
+ * Submitted: 12/5/17
+ * Comment: Test suite and sample run attached
+ * @author Tapan Soni
+ * @author John Stranahan
+ * @version 2017.4.12
+ */
+
 public class ListArrayBasedGeneric<T> implements ListInterfaceGeneric<T>
 {
 	private T [] items;
