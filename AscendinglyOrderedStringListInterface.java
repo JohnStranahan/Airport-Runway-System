775c6e5b625450417b479f8e52eff2e9f878dbad
Tapan Soni <32372516+TapanSoni@users.noreply.github.com>
2017-12-04 20:33:01 -0500

    Update AscendinglyOrderedStringListInterface.java

--- AscendinglyOrderedStringListInterface.java
+++ AscendinglyOrderedStringListInterface.java
@@ -1,3 +1,14 @@
+/**
+ * Purpose: Data Structure and Algorithms Fall 2017 Final Project
+ * Status: Complete and throughly tested
+ * Last Update: 12/4/17
+ * Submitted: 12/5/17
+ * Comment: Test suite and sample run attached
+ * @author Tapan Soni
+ * @author John Stranahan
+ * @version 2017.4.12
+ */
+
 public interface AscendinglyOrderedStringListInterface 
 {
 	boolean isEmpty();
@@ -7,4 +18,4 @@ public interface AscendinglyOrderedStringListInterface
 	void remove(int index) throws ListIndexOutOfBoundsException;
 	int search(String item);
 	void clear();
-} // END AscendinglyOrderedStringListInterface
+} // END AscendinglyOrderedStringListInterface {}
