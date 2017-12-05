bf8dc8263823103723288e4b1ae48b4ff2c1ffc7
JohnStranahan <jasballpythons@msn.com>
2017-12-04 22:13:27 -0500

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -15,43 +15,72 @@ public class Plane
 	private String destination;
 	private String runway;
 
+	/**
+	 * Plane() constructor method
+	 * @param flightNumber
+	 * @param destination
+	 * @param runway
+	 */
 	public Plane(String flightNumber, String destination, String runway) 
 	{
 		this.flightNumber = flightNumber;
 		this.destination = destination;
 		this.runway = runway;
 	} // END Plane() Constructor
-
+	
+	/**
+	 * getFlightNumber() accessor method
+	 * @return flightNumber
+	 */
 	public String getFlightNumber()
 	{
 		return flightNumber;
 	} // END getFlightNumber() method
 
+	/**
+	 * setFlightNumber() mutator method
+	 * @param flightNumber
+	 */
 	public void setFlightNumber(String flightNumber)
 	{
 		this.flightNumber = flightNumber;
 	} // END setFlightNumber() method
-
+	/**
+	 * getDestination() accessor method
+	 * @return destination
+	 */
 	public String getDestination()
 	{
 		return destination;
 	} // END getDestination() method
-
+	/**
+	 * setDestination() mutator method
+	 * @param destination
+	 */
 	public void setDestination(String destination) 
 	{
 		this.destination = destination;
 	} // END setDestinatio() method
-
+	/**
+	 * setDestination() mutator method
+	 * @return runway
+	 */
 	public String getRunway() 
 	{
 		return runway;
 	} // END getRunway() method
-
+	/**
+	 * setRunway() mutator method
+	 * @param runway
+	 */
 	public void setRunway(String runway) 
 	{
 		this.runway = runway;
 	} // END setRunway() method
-	
+	/**
+	 * toString() method for Plane object
+	 * @return String
+	 */
 	public String toString()
 	{
 		return "Flight " + flightNumber + " to " + destination;
