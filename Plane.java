8418fe7d45fc7c42d4f71662dbff03af45bb5cce
Tapan Soni <32372516+TapanSoni@users.noreply.github.com>
2017-12-04 20:35:19 -0500

    Update Plane.java

--- Plane.java
+++ Plane.java
@@ -1,3 +1,14 @@
+/**
+ * Purpose: Data Structure and Algorithms Fall 2017 Final Project
+ * Status: Complete and throughly tested
+ * Last Update: 12/4/17
+ * Submitted: 12/5/17
+ * Comment: Test suite and sample run attached
+ * @author Tapan Soni
+ * @author John Stranahan
+ * @version 2017.4.12
+ */
+
 public class Plane 
 {
 	private String flightNumber;
@@ -19,30 +30,30 @@ public class Plane
 	public void setFlightNumber(String flightNumber)
 	{
 		this.flightNumber = flightNumber;
-	}
+	} // END setFlightNumber() method
 
 	public String getDestination()
 	{
 		return destination;
-	}
+	} // END getDestination() method
 
 	public void setDestination(String destination) 
 	{
 		this.destination = destination;
-	}
+	} // END setDestinatio() method
 
 	public String getRunway() 
 	{
 		return runway;
-	}
+	} // END getRunway() method
 
 	public void setRunway(String runway) 
 	{
 		this.runway = runway;
-	}
+	} // END setRunway() method
 	
 	public String toString()
 	{
 		return "Flight " + flightNumber + " to " + destination;
-	}
+	} // END toString() method
 } // END CLASS Plane {}
