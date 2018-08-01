5c5b81dffb93e21779618012441cef3c6b2f11dc
JohnStranahan <jasballpythons@msn.com>
2018-07-31 20:41:09 -0400

    Delete QueueException.java

--- QueueException.java
+++
@@ -1,8 +0,0 @@
-
-public class QueueException extends RuntimeException
-{
-	public QueueException(String s)
-	{
-		super(s);
-	} // END QueueException Default Constructor
-} // END CLASS QueueException {}
